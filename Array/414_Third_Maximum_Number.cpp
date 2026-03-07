76e9e36a74a852330a55c03c8bcbf7a05a8a5508
Sahilw17 <sahilwaghole1784@gmail.com>
2026-03-07 16:57:03 +0530

    DSA

---
+++ Array/414_Third_Maximum_Number.cpp
@@ -0,0 +1,67 @@
+/*
+Given an integer array nums, return the third distinct maximum number in this array.
+If the third maximum does not exist, return the maximum number.
+
+Example 1:
+Input: nums = [3,2,1]
+Output: 1
+Explanation:
+The first distinct maximum is 3.
+The second distinct maximum is 2.
+The third distinct maximum is 1.
+
+Example 2:
+Input: nums = [1,2]
+Output: 2
+Explanation:
+The first distinct maximum is 2.
+The second distinct maximum is 1.
+The third distinct maximum does not exist, so the maximum (2) is returned instead.
+
+Example 3:
+Input: nums = [2,2,3,1]
+Output: 1
+Explanation:
+The first distinct maximum is 3.
+The second distinct maximum is 2 (both 2's are counted together since they have the same value).
+The third distinct maximum is 1.
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int>v={2,2,3,1};
+    int m2,m3=INT_MIN;
+    int m1=v[0];
+    if(v.size()<3){
+        if(m1>v[1]){
+            cout << m1;
+        }
+        else{
+            cout<< v[1];
+        }
+    }
+    else{
+            
+
+        cout << m3;
+    }
+    
+
+}
+
+
+/*
+set<int>st;
+
+    for(int i=0;i<v.size();i++){
+       st.insert(v[i]);
+    }
+    if(st.size()>=3){
+        cout << *prev(prev(prev(st.end())));
+    }
+    else{
+        cout << *prev(st.end());
+    }
+*/
